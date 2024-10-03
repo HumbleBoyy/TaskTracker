b58ce9e76c8baf7bf14d4247949db720ee24d417
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2024-10-03 11:03:55 +0500

    New Commit

--- src/Components/Hooks/UseStateHook.jsx
+++ src/Components/Hooks/UseStateHook.jsx
@@ -2,15 +2,33 @@ import React, { useState } from 'react'
 import "./hooks.css"
 const UseStateHook = () => {
     const [color, setColor] = useState('red')
+    const[increemant, setIncreemant] = useState(0)
+    const[decreement, setDecreement] = useState(0)
+    const[name, setName] = useState('Username')
   return (
     <>
-      <h1 className='hooks_ttle'>My Favorite color is {color}</h1>
+      <h1 className='hooks_ttle'>My Favorite color is "{color}"</h1>
+      <div className="container">
       <button className='btn_hooks' onClick={()=> setColor("Blue")}>Blue</button>
       <button className='btn_hooks red' onClick={()=> setColor("Red")}>Red</button>
       <button className='btn_hooks yellow' onClick={()=> setColor("Yellow")}>Yellow</button>
       <button className='btn_hooks pink' onClick={()=> setColor("Pink")}>Pink</button>
       <button className='btn_hooks green' onClick={()=> setColor("Green")}>Green</button>
-      <button className='btn_hooks white' onClick={()=> setColor("White")}>Blue</button>
+      <button className='btn_hooks white' onClick={()=> setColor("White")}>White</button>
+      </div>
+      <div className="counter">
+      <button onClick={(e)=>setIncreemant(e.target.value ++)} className='btn_hooks'>Increase Positive Number: {increemant}</button>
+      <button onClick={(e)=>setDecreement(e.target.value --)} className='btn_hooks'>Decrease Negative Number: {decreement}</button>
+      </div>
+
+      <h2 className='hooks_ttle'>My Name is "{name}"</h2>
+      <div className='names_container'>
+        <button className='namesBtn' onClick={()=> setName("Asadulloh")}>Asadulloh</button>
+        <button className='namesBtn' onClick={()=> setName("Asadbek")}>Asadbek</button>
+        <button className='namesBtn' onClick={()=> setName("John")}>John</button>
+        <button className='namesBtn' onClick={()=> setName("Tom")}>Tom</button>
+        <button className='namesBtn' onClick={()=> setName("Tommy")}>Tommy</button>
+      </div>
     </>
   )
 }
